a2f9071def76525f836f23c844e982a857e2552e
Murilo Rodrigues dos Anjos <muriloanjos99@gmail.com>
2024-09-18 22:43:35 -0300

    :recycle: refactor: Envolve a tag img com a tag picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="page_container">
       <header className="header_container">
-        <img src="logo.svg" alt="Logo da FocalPoint" />
+        <picture>
+          <img src="logo.svg" alt="Logo da FocalPoint" />
+        </picture>
         <h2 className="header_title">Bem-vindo de volta, Marcus</h2>
         <p className="header_date">Segunda, 01 de dezembro de 2025</p>
       </header>
