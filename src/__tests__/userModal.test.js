7e100d559651891aa1af31233d5ce3e5673e09e0
Murilo Rodrigues dos Anjos <muriloanjos99@gmail.com>
2024-09-20 09:29:44 -0300

    :test_tube: test: Implementa testes para o modal de nome

---
+++ src/__tests__/userModal.test.js
@@ -0,0 +1,57 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import UserModal from '../components/userModal/userModal';
+
+describe('O componente UserModal', () => {
+  const tasks = { open: [], completed: [] };
+  const setTasks = jest.fn();
+  const setSelectedTask = jest.fn();
+  const handleModalOpenClose = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Salva o nome inserido pelo usuário', () => {
+    const setUser = jest.fn();
+    const handleUserModalOpenClose = jest.fn();
+
+    render(
+      <UserModal
+        isModalOpen={true}
+        setUser={setUser}
+        handleUserModalOpenClose={handleUserModalOpenClose}
+      />
+    );
+
+    const input = screen.getByPlaceholderText('Digite');
+    fireEvent.change(input, { target: { value: 'Murilo' } });
+
+    const loginButton = screen.getByText('Confirmar');
+    fireEvent.click(loginButton);
+
+    expect(setUser).toHaveBeenCalledWith('Murilo');
+    expect(localStorage.getItem('focalpoint_user')).toBe('Murilo');
+    expect(handleUserModalOpenClose).toHaveBeenCalled();
+  });
+
+  it('Remove o nome salvo pelo usuário caso este queira trocar', () => {
+    const setUser = jest.fn();
+    const handleUserModalOpenClose = jest.fn();
+
+    render(
+      <UserModal
+        isModalOpen={true}
+        user={"Murilo"}
+        setUser={setUser}
+        handleUserModalOpenClose={handleUserModalOpenClose}
+      />
+    );
+
+    const changeUserButton = screen.getByText('Mudar usuário');
+    fireEvent.click(changeUserButton);
+
+    expect(setUser).toHaveBeenCalledWith(null);
+    expect(localStorage.getItem('focalpoint_user')).toBe(null);
+    expect(handleUserModalOpenClose).toHaveBeenCalled();
+  });
+});
