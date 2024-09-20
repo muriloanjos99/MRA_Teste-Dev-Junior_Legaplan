35c64f50464be01fffa82f5da71e3f112de1890e
Murilo Rodrigues dos Anjos <muriloanjos99@gmail.com>
2024-09-20 04:03:42 -0300

    ✨ feat: Integra os componentes do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import "./page.scss";
+import TasksType from "@/types/tasksType";
+import TaskType from "@/types/taskType";
+import TaskItem from "@/components/taskItem/taskItem";
+import NewTaskModal from "@/components/newTaskModal/newTaskModal";
+import DeleteTaskModal from "@/components/deleteTaskModal/deleteTaskModal";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<TasksType>({ open: [], completed: [] });
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isDelModalOpen, setIsDelModalOpen] = useState(false);
+  const [selectedTask, setSelectedTask] = useState<TaskType | null>(null);
+
+  useEffect(() => {
+    const currentTasks: TasksType = {
+      open: [...tasks.open],
+      completed: [...tasks.completed],
+    };
+
+    const storedOpenTasks = localStorage.getItem('open_tasks');
+    const storedCompletedTasks = localStorage.getItem('completed_tasks');
+
+    if (storedOpenTasks) {
+      currentTasks.open.push(...(JSON.parse(storedOpenTasks) as TaskType[]));
+    }
+
+    if (storedCompletedTasks) {
+      currentTasks.completed.push(...(JSON.parse(storedCompletedTasks) as TaskType[]));
+    }
+
+    setTasks(currentTasks);
+  }, []);
+
+  const handleAddModalOpenClose = () => {
+    setIsAddModalOpen(!isAddModalOpen);
+  };
+
+  const handleDelModalOpenClose = () => {
+    setIsDelModalOpen(!isDelModalOpen);
+  };
+
   return (
     <div className="page_container">
       <header className="header_container">
@@ -12,11 +53,21 @@ export default function Home() {
       </header>
       <main className="tasks_container">
         <p className="open_tasks_title">Suas tarefas de hoje</p>
-        <ul className="open_tasks"></ul>
+        <ul className="open_tasks">
+          {tasks.open.map((task, index) => (
+            <TaskItem key={index} task={task} setSelectedTask={setSelectedTask} openDeleteModal={handleDelModalOpenClose} setTasks={setTasks} tasks={tasks} />
+          ))}
+        </ul>
         <p className="completed_tasks_title">Tarefas finalizadas</p>
-        <ul className="completed_tasks"></ul>
+        <ul className="completed_tasks">
+          {tasks.completed.map((task, index) => (
+            <TaskItem key={index} task={task} setSelectedTask={setSelectedTask} openDeleteModal={handleDelModalOpenClose} setTasks={setTasks} tasks={tasks} />
+          ))}
+        </ul>
       </main>
-      <button className="new_task_button">Adicionar nova tarefa</button>
+      <button className="new_task_button" onClick={handleAddModalOpenClose}>Adicionar nova tarefa</button>
+      <NewTaskModal isModalOpen={isAddModalOpen} handleModalOpenClose={handleAddModalOpenClose} tasks={tasks} setTasks={setTasks} />
+      <DeleteTaskModal isModalOpen={isDelModalOpen} handleModalOpenClose={handleDelModalOpenClose} tasks={tasks} setTasks={setTasks} selectedTask={selectedTask} setSelectedTask={setSelectedTask} />
     </div>
   );
 }
